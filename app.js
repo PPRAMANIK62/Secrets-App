860e282acb619bc1ff816713e349147e8f414c94
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2023-10-07 18:34:39 +0530

    Level 2 : Included Environment Variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
@@ -17,8 +18,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
